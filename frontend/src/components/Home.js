7133c38c5012c5891d37138aa6679873b6010e97
Alireza <alireza27163@gmail.com>
2021-09-20 22:28:30 +0300

    Search added Reactjs

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,77 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Fragment, useEffect, useState }  from "react";
+import MetaData from "./layout/MetaData";
+import {useDispatch, useSelector} from 'react-redux';
+import {getProducts} from "../action/productActions";
+import Product from "./product/Product";
+import Loader from "./layout/Loader"
+import { useAlert } from 'react-alert';
+import Pagination from "react-js-pagination";
+function Home({match}) {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products)
+
+  const keyword = match.params.keyword;
+  useEffect(() => {
+    if(error){
+      
+      return alert.error(error)
+
+    }
+    dispatch(getProducts(keyword, currentPage));
+
+  }, [dispatch, alert, error,keyword, currentPage])
+
+  function setCurrentPageNo(pageNumber){
+    setCurrentPage(pageNumber)
+  }
+
+
+  return (
+    <Fragment>
+
+      {loading ? <Loader /> :(
+        <Fragment>
+          <MetaData title={'Buy Best Products Online'}/>
+      <h1 id="products_heading">Latest Products</h1>
+
+      <section id="products" className="container mt-5">
+        <div className="row">
+          {products && products.map(product => (
+
+            <Product key={product._id} product={product}/>
+          ))}
+        </div>
+      </section>
+        {resPerPage <= productsCount && (
+          <div className="d-flex justify-content-center mt-5">
+          <Pagination
+          activePage ={currentPage}
+          itemsCountPerPage={resPerPage}
+          totalItemsCount={productsCount}
+          onChange={setCurrentPageNo}
+          nextPageText={'Next'}
+          prevPageText={'Prev'}
+          firstPageText={'First'}
+          lastPageText={'Last'}
+          itemClass="page-item"
+          linkClass="page-link"
+          
+          />
+  
+        </div>
+        ) }
+      
+
+        </Fragment>
+      )}
+        
+    </Fragment>
+  );
+}
+
+export default Home;
---
+++ server.js
@@ -0,0 +1,34 @@
+const app = require("./app");
+const connectDatabase = require("./config/database");
+
+const dotenv = require("dotenv");
+
+// Handle the uncought exceptions       => this code should be on the top
+process.on("uncaughtException", err =>{
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting down the server due to Uncought Exceptions");
+    process.exit(1);
+})
+
+
+
+// setting up config file
+dotenv.config({path: "config/config.env"});
+
+// Connecting to database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, ()=>{
+    console.log(`Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+});
+
+// Handle unhandled Promised rejections
+process.on("unhandledRejection", err =>{
+    console.log(`ERROR: ${err.stack}`);
+    console.log("Shutting down the server due to unhandled Promise rejection");
+    server.close(()=>{
+        process.exit(1)
+    }) 
+})
+
+
